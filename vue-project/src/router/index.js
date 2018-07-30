712f9f18b8a68ea13a15d329b79e11a01f074d40
chenlian <chenlian@dst70108.cn1.global.ctrip.com>
2018-07-30 17:40:38 +0800

    ssr - document

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -19,35 +19,46 @@ const routeParams = {
     name: 'Flex',
     component: Flex,
     meta: {title: 'flex test'}
-  }
-  ]
-}
-const router = new Router(routeParams)
-
-/**
- * 进入后触发，已经跳转，没有next方法
- */
-router.afterEach((to) => {
-  document.title = to.meta.title
-})
-/**
- * 跳转前，全局前置路由
- */
-router.beforeEach((to, from, next) => {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(resolve(11), 20)
-  })
-  promise.then(res => {
-    if (res === 11) {
-      next()
-    } else {
-      next()
+  },{
+    path:'*',
+    name:'HelloWorld',
+    component: HelloWorld,
+    meta: {
+      title: '首页'
     }
-  })
-})
+  }]
+}
 
 // export default router
 
 export function createRouter() {
-  return new Router(routeParams)
+  const router = new Router(routeParams)
+
+  /**
+   * 进入后触发，已经跳转，没有next方法
+   */
+  router.afterEach((to) => {
+    // 用于区分客户端和服务端的，服务端是没有window，document对象
+    if(typeof document !=='undefined'){
+      document.title = to.meta.title
+      console.log(to,document)
+    }
+  })
+  /**
+   * 跳转前，全局前置路由
+   */
+  // router.beforeEach((to, from, next) => {
+  //   const promise = new Promise((resolve, reject) => {
+  //     setTimeout(resolve(11), 20)
+  //   })
+  //   document.title = 'test'
+  //   promise.then(res => {
+  //     if (res === 11) {
+  //       next()
+  //     } else {
+  //       next()
+  //     }
+  //   })
+  // })
+  return router
 }
